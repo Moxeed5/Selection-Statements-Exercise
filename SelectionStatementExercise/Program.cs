365f02bce368d4b94133d741be86676629225066
Max Morgan <maxmmorgan93@gmail.com>
2022-11-29 19:39:36 -0600

    update: added guess limit and # of guesses left

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,39 +1,58 @@
-﻿namespace SelectionStatementExercise
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            
+           
 
             var r = new Random();
 
-            var favNum = r.Next(1,1000);
+            var favNum = r.Next(1,25);
 
-            bool correct = false;
+            int maxGuess = 4;
+            
+            Console.WriteLine("You have 5 guesses...");
 
-
-            while (!correct)
+            for (int i = 0; i < 5; i++)
             {
+                    //Here is how this for loop is working: 
+                    // The loop will iterate 5 times starting at 0 (0,1,2,3,4) 
+                    //first, the loop will check if i (the loop counter), is the same as maxGuesses (which is set to 4)
+                    //if that condition is not true, the loop checks to see if the userINput is lower or higher, or correct. 
+                    //if none of those conditions are met, the loops starts over checking the first if statement, and then looping through the rest
+                    //the two ways to break the loop are i being == maxGuesses, or guessing the number correctly.
 
-                Console.WriteLine("Guess my favorite number: ");
+                    Console.WriteLine("Guess my favorite number: ");
 
-                var userInput = int.Parse(Console.ReadLine());
+                    var userInput = int.Parse(Console.ReadLine());
 
-
-                if (userInput > favNum)
-                {
-                    Console.WriteLine("Too high try again");
-                }
-                else if (userInput < favNum)
-                {
-                    Console.WriteLine("Too low, try again");
-                } 
-                
-
+                    if (i == maxGuess)
+                    {
+                        Console.WriteLine("Out of Guesses");
+                        break;
+                    }
+                    if (userInput > favNum)
+                    {
+                        Console.WriteLine($"Too high, you have {4-i} guesses left... try again");
+                    }
+                    else if (userInput < favNum)
+                    {
+                        Console.WriteLine($"Too low, you have {4-i} guesses left... try again");
+                    }
+                    
+                    else 
+                    {
+                        Console.WriteLine("Correct!");
+                        
+                        break;
+                    }
+                    
             }
 
-            Console.WriteLine("Correct");
+            
         }
 
         
